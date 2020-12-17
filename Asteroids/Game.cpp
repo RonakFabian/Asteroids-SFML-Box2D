d8158f47f8882b2e1778ca8c8cd10d7e66ee0e08
RonakFabian <ronakmanfabian@gmail.com>
2020-12-17 19:26:32 +0530

    Box2d physics

---
+++ Asteroids/Game.cpp
@@ -0,0 +1,35 @@
+// #include <box2d/box2d.h>
+// #include <SFML/Graphics.hpp>
+// #include "Scene.h"
+//
+//
+// int main()
+// {
+// 	auto my_scene= new Scene;
+//
+// 	sf::RenderWindow window(sf::VideoMode(1080, 720), "SFML works!");
+// 	
+//
+// 	my_scene->SetWindow(&window);
+// 	
+//
+// 	
+//
+// 	while (window.isOpen())
+// 	{
+// 		sf::Event event;
+// 		while (window.pollEvent(event))
+// 		{
+// 			if (event.type == sf::Event::Closed)
+// 				window.close();
+// 		}
+// 		
+//
+//
+// 		window.clear();
+// 		my_scene->Render(&window);
+// 		window.display();
+// 	}
+//
+// 	return 0;
+// }
